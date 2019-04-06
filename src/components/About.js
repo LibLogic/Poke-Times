0b123f0cb78705ad8892e932614f63a4ae42b649
Tom Hodgkinson <hdgknsn@gmail.com>
2019-04-06 06:45:36 -0400

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint dignissimos distinctio, sunt pariatur cum voluptates similique amet ullam expedita nostrum ipsam doloribus. Labore et at magnam natus quam, porro adipisci.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint dignissimos distinctio, sunt pariatur cum voluptates similique amet ullam expedita nostrum ipsam doloribus. Labore et at magnam natus quam, porro adipisci.</p>
+    </div>
+  )
+}
+
+export default Home;
